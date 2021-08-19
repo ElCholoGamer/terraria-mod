62808c76b5e7fd0c67aa63759bf819e2b46f1c08
ElCholoGamer <josedanielgrayson@gmail.com>
2021-08-19 16:05:31 -0400

    add supreme manipulator

---
+++ Items/SupremeManipulator.cs
@@ -0,0 +1,44 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CholosRandomMod.Items
+{
+    class SupremeManipulator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Supreme Manipulator");
+            Tooltip.SetDefault("Acts as most crafting stations and an altar\n'A crafting station used by the gods'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.noMelee = true;
+            item.createTile = ModContent.TileType<Tiles.SupremeManipulator>();
+            item.maxStack = 20;
+            item.value = 1250000;
+            item.useTime = 10;
+            item.useAnimation = 15;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.autoReuse = true;
+            item.useTurn = true;
+            item.rare = ItemRarityID.Purple;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+
+            int[] workStations = Tiles.SupremeManipulator.workStations;
+            for (int i = 0; i < workStations.Length; i++)
+            {
+                recipe.AddIngredient(workStations[i]);
+            }
+
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/SupremeManipulator.cs
@@ -0,0 +1,60 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace CholosRandomMod.Tiles
+{
+    class SupremeManipulator : ModTile
+    {
+        public static int[] workStations = new int[] {
+                TileID.WorkBenches,
+                TileID.MythrilAnvil,
+                TileID.AdamantiteForge,
+                TileID.AlchemyTable,
+                TileID.Sinks,
+                TileID.Bookcases,
+                TileID.CookingPots,
+                TileID.TinkerersWorkbench,
+                TileID.ImbuingStation,
+                TileID.HeavyWorkBench,
+                TileID.DemonAltar,
+                TileID.CrystalBall,
+                TileID.Autohammer,
+                TileID.LunarCraftingStation
+        };
+
+        public override void SetDefaults()
+        {
+            Main.tileLavaDeath[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 18 };
+            TileObjectData.newTile.Origin = new Point16(1, 2);
+            TileObjectData.addTile(Type);
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Supreme Manipulator");
+            AddMapEntry(new Color(78, 255, 51), name);
+
+            disableSmartCursor = true;
+            dustType = DustID.GreenFairy;
+
+            adjTiles = workStations;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3; // Create 1 dust if hit, 3 if tile broken
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<Items.SupremeManipulator>());
+        }
+    }
+}
