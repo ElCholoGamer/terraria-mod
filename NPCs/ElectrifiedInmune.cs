d2ed6c22f4177abcde35ad0ac59da951e31559c1
ElCholoGamer <josedanielgrayson@gmail.com>
2021-08-19 08:47:01 -0400

    make tesla turret and martian engineer inmune to electrified

---
+++ NPCs/ElectrifiedInmune.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CholosRandomMod.NPCs
+{
+    class ElectrifiedInmune : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            if (npc.type == NPCID.MartianTurret || npc.type == NPCID.MartianEngineer)
+            {
+                npc.buffImmune[BuffID.Electrified] = true;
+            }
+        }
+    }
+}
