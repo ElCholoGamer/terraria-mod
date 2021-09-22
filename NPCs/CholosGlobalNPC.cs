c48f1e175ea54b225adfc7bfcab9176300146193
ElCholoGamer <josedanielgrayson@gmail.com>
2021-09-22 16:31:42 -0500

    add lancer painting

---
+++ NPCs/CholosGlobalNPC.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CholosRandomMod.Items.Placeable;
+
+namespace CholosRandomMod.NPCs
+{
+    public class CholosGlobalNPC : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            if (type == NPCID.Clothier)
+            {
+                shop.item[nextSlot++].SetDefaults(ModContent.ItemType<LancerPainting>());
+            }
+        }
+    }
+}
---
+++ Tiles/LancerPainting.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+
+namespace CholosRandomMod.Tiles
+{
+    public class LancerPainting : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.Origin = new Point16(2, 2);
+            TileObjectData.newTile.Width = 6;
+            TileObjectData.newTile.Height = 4;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.addTile(Type);
+
+            disableSmartCursor = true;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Painting");
+            AddMapEntry(new Color(120, 85, 60), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 48, 48, ModContent.ItemType<Items.Placeable.LancerPainting>());
+        }
+    }
+}
